843b49b8e04da3c824a764139d2f593169f8f7e0
Troy Tao <troy@faire.com>
2025-01-07 17:35:06 -0500

    Add test code

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: true,
+  webpack: (config, context) => {
+    if (context.isServer) {
+      if (context.dev) {
+        config.devtool = "eval-source-map";
+      } else {
+        config.devtool = "source-map";
+      }
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
